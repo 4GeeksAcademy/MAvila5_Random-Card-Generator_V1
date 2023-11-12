3921978d42fe1ea50376ef81e8aa29fd39c9de7b
Mateo Avila <mateoavila@Mathews-MacBook-Air-2.local>
2023-11-12 16:27:00 -0500

    Created Card, Styles, and JS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.addEventListener('DOMContentLoaded', function () {
+  const suits = ['♥', '♠', '♣', '♦'];
+  const cardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+
+  function generateRandomSuit() {
+    return getRandomElement(suits);
+  }
+
+  function generateRandomValue() {
+    return getRandomElement(cardValues);
+  }
+
+  function generateRandomCard() {
+    const randomSuit = generateRandomSuit();
+    const randomValue = generateRandomValue();
+
+    document.querySelector('.topSuite').textContent = randomSuit;
+    document.querySelector('.number').textContent = randomValue;
+    document.querySelector('.bottomSuite').textContent = randomSuit;
+  }
+
+  // Run the function on page load
+  document.addEventListener('DOMContentLoaded', generateRandomCard);
+
+  // Bind the button click to generate a new card
+  document.querySelector('.btn').addEventListener('click', generateRandomCard);
+});
+
